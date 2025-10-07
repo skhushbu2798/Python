7e7e051329ce40dbc22c3da4fc8c4cb162245b01
skhushbu2798 <skhushbu2798@gmail.com>
2025-10-08 01:04:58 +0530

    Complete Python First Assignment

--- Assignment1.py
+++ Assignment1.py
@@ -1,1 +1,16 @@
-print('Hello khushi')
+#Task 1
+a=input('Enter the first number:')
+b=input('Enter the second number:')
+a=int(a)
+b=int(b)
+
+print('\nAddition:',a+b)
+print('Subtraction:',a-b)
+print('Multiplication:',a*b)
+print('Division:',a/b)
+
+#Task 2
+x=input('\n\nEnter the First Name:')
+y=input('Enter the Last Name:')
+
+print('\nHello, '+ x,y+'!' +' Welcome to the python program.')
